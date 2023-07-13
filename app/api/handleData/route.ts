f3bd3118b2b271d1756d8ac8465f4a90524ad58f
Kenniel-Torres <kennieltorres@gmail.com>
2023-07-12 21:37:39 -0400

    Removed types interface. Started rendering categories.

--- app/api/handleData/route.ts
+++ app/api/handleData/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
  
 export async function GET() {
-    const url = 'https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1//?size=10'
+    const url = 'https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1//?size=2&date_received_min=2022-01-01&date_received_max=2022-12-30'
     const res = await fetch(url,
         {
             method: 'GET',
@@ -12,5 +12,5 @@ export async function GET() {
     )
     const data = await res.json()
   
-    return NextResponse.json(JSON.stringify({ data: data }))
+    return NextResponse.json({ data: data })
 }
--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { useQuery } from "@tanstack/react-query"
-import { Complaints } from "@/types/complaints";
+import { useState } from "react";
 
 export default function statsPage(){
+    const [categoryState, setCategory] = useState("None")
 
     const fetchAllData = async () => {
         try {
@@ -16,7 +17,6 @@ export default function statsPage(){
                 },
             )
             const data = await res.json()
-            // console.log(data) // DEBUG
             return data
         }
         catch (err) {
@@ -25,7 +25,7 @@ export default function statsPage(){
         }
     };
 
-    const {data, isLoading, error} = useQuery<Complaints, Error>({
+    const {data, isLoading, error} = useQuery({
         queryKey: ['allData'],
         queryFn: fetchAllData,
     });
@@ -46,10 +46,25 @@ export default function statsPage(){
 
     return(
         <>
-            <div>Statistics</div>
-            <p>Response =  
-                {JSON.stringify(data)}
-            </p>
+            <div id="stat-container" className="mx-4 md:mx-10">
+                <div className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">Statistics</div>
+                <label htmlFor="#categorySelector">Choose a category:</label>
+                <div id="placeholder" className="flex flex-col flex-wrap">
+                    <select id="categorySelector" className="max-w-[50%] md:max-w-[25%] outline rounded ml-2" onChange={(e) => {setCategory(e.target.value); console.log(e.target.value)}} defaultValue={"None"}> 
+                        <option value={"None"}>---</option>
+                        {Object.values(data?.data?.aggregations?.product?.product?.buckets).map((item:any) =>
+                            <option key={item.key} value={item.key}>{item?.key}.</option>
+                        )}
+                    </select>
+                </div>
+                {/* Example of conditonal rendering. */}
+                <div>
+                    {(categoryState == "None") &&
+                        <p>test</p>
+                    }
+                </div>
+
+            </div>
         </>
     )
 }
